3c8bb560d7d37f2c2070877089c4789434758faf
U3cSeroH1 <unkio0602@gmail.com>
2019-11-28 16:16:35 +0900

    omg

---
+++ SteamVR_USE_Proj/Assets/ClimbingRigHand.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Valve.VR;
+public class ClimbingRigHand : MonoBehaviour
+{
+    public SteamVR_Input_Sources Hand;
+    public int TouchedCount;
+    public bool grabbing;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Climbable"))
+        {
+            TouchedCount++;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Climbable"))
+        {
+            TouchedCount--;
+        }
+    }
+}
