dc95f8c94215cf2c848d943a76e2d270de2484f3
U3cSeroH1 <unkio0602@gmail.com>
2020-01-31 17:19:32 +0900

    k4

---
+++ SteamVR_USE_Proj/Assets/吸着/StickyFinger_Invent.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using Valve.VR.InteractionSystem;
+using System;
+
+public class StickyFinger_Invent : MonoBehaviour
+{
+
+    GameObject _parent;
+
+    void Start()
+    {
+        StartCoroutine("OnTriggerStay");
+    }
+
+    void Update()
+    {
+    }
+
+    IEnumerator OnTriggerStay(Collider other)
+    {
+
+        if (other.transform.parent.gameObject.GetComponent<Interactable>())
+        {
+            _parent = other.transform.parent.gameObject;
+
+            if (!_parent.gameObject.GetComponent<Interactable>().attachedToHand)
+            {
+                yield return new WaitForSeconds(0.001f);
+                _parent.gameObject.transform.parent = transform.parent;
+
+
+                _parent.gameObject.transform.position = this.transform.position;
+                _parent.gameObject.transform.rotation = this.transform.rotation;
+
+
+                _parent.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+
+            }
+            else
+            {
+                _parent.gameObject.transform.parent = null;
+                _parent.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+            }
+
+
+        }
+
+    }
+
+
+
+}
